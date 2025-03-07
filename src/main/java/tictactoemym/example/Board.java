c3d151e13a0882d2cc822438d48f39885cd53a0a
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-03-07 11:40:18 +0100

    Create initial empty board

---
+++ src/main/java/tictactoemym/example/Board.java
@@ -0,0 +1,22 @@
+package tictactoemym.example;
+
+import java.util.Scanner;
+
+public class Board {
+
+  public static void main(String[] args){
+ 
+    Scanner board = new Scanner(System.in);
+
+    System.out.println("_ " + " _ "  +  " _");
+    System.out.println("_ " + " _ "  +  " _");
+    System.out.println("_ " + " _ "  +  " _");
+    board.close();
+    
+  }
+
+                          
+                          
+                          
+}
+    
